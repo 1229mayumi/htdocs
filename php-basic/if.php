fa3cabf1ca69c9c9d0e2d91faaec029ecf931882
1229mayumi <letrico1229@yahoo.co.jp>
2024-03-13 06:27:38 +0900

    if.php html定型分あり

---
+++ php-basic/if.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    //算術演算子を使った場合の戻り値を出力する
+    echo 45 + 18;
+    //改行する
+    echo '<br>';
+    //比較演算子を使った場合の戻り値を出力する
+    var_dump(45 > 18);
+    ?>
+  </p>
+  
+</body>
+</html>
